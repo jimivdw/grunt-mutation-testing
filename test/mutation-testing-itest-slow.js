fef43b9b3b4f7db184b5a9a0f67daaad1cad84bd
Martin Koster <martin.koster@ing.nl>
2015-02-23 11:32:58 +0100

    fixed slow test by adding a new 'expected' file. testing grunt.txt against mocha.txt makes no sense as mutationTest:grunt tests more files than mutationTest:mocha

--- test/mutation-testing-itest-slow.js
+++ test/mutation-testing-itest-slow.js
@@ -1,9 +1,11 @@
-'use strict';
+(function() {
+    'use strict';
 
-var assertExpectedReport = require('./test-utils').assertExpectedReport;
+    var assertExpectedReport = require('./test-utils').assertExpectedReport;
 
-describe('Mutation Testing', function () {
-    it('flags all mutations for which a grunt run returns a good status', function () {
-        assertExpectedReport('tmp/grunt.txt', 'test/expected/mocha.txt');
+    describe('Mutation Testing', function () {
+        it('flags all mutations for which a grunt run returns a good status', function () {
+            assertExpectedReport('tmp/grunt.txt', 'test/expected/grunt.txt');
+        });
     });
-});
+})();
