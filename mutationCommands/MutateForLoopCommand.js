9bbc1fc47c7faf44cd116286c2d0c8b4ceac9da9
martin <paysdoc@gmail.com>
2015-03-30 10:27:39 +0200

    added jsdoc and other comments for clarity

--- mutationCommands/MutateForLoopCommand.js
+++ mutationCommands/MutateForLoopCommand.js
@@ -10,7 +10,7 @@ function MutateForLoopCommand (src, subTree, callback) {
 
 MutateForLoopCommand.prototype.execute = function () {
     return ([
-        // only return the 'body' and 'init' nodes as mutating either 'test' or 'update' nodes introduce too great a risk of resulting in an infinite loop
+        // only return the 'body' and 'init' nodes as mutating either 'test' or 'update' nodes introduces too great a risk of resulting in an infinite loop
         {node: this._astNode.init, parentMutationId: this._parentMutationId, loopVariables: this._loopVariables},
         {node: this._astNode.body, parentMutationId: this._parentMutationId, loopVariables: this._loopVariables}
     ]);
